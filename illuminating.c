3b06b870896e25a61c138ec0f4ad3ea6c76b2933
Han Liu <liuh3410@muskrat.cs.umanitoba.ca>
2017-03-02 03:22:48 -0600

    this is assignment2

--- illuminating.c
+++ illuminating.c
@@ -4,4 +4,34 @@
 #include <ctype.h>
 #include <assert.h>
 
+struct DUNGEON{
+	char title[500];
+	char array[1000][1000];
+	int steps;
+};
 
+typedef struct DUNGEON Dungeon;
+
+FILE *loadFile(char *name);
+
+main(int argc, char *argv[]){
+	FILE *file = NULL;
+	file = loadFile(argv[1]);
+
+	return EXIT_SUCCESS;
+}
+
+FILE *loadFile(char *name){
+	FILE *file = NULL;
+	assert(name != NULL);
+	if(name != NULL){
+		file = fopen(name, "r");
+		assert(file != NULL);
+	}
+	
+	if(file == NULL){
+		printf("Open file failed");
+	}
+
+	return file;
+}
